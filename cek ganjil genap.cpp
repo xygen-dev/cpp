ce262a935a16f6f3a938c9bc75bd6c11e638e715
xygen-dev <60088151+xygen-dev@users.noreply.github.com>
2020-01-20 14:25:56 +0700

    Create cek ganjil genap.cpp
    Program menentukan bilangan ganjil dan genap

---
+++ cek ganjil genap.cpp
@@ -0,0 +1,20 @@
+#include <iostream.h>
+#include <conio.h>
+
+int main ()
+{
+	int a;
+
+   cout << "Masukan Nilai A: ";
+   cin  >> a;
+
+   if (a % 2 == 1)
+   {
+   	cout << "A Merupakan Angka Ganjil";
+   }
+   else
+   {
+   	cout << "A Merupakan Angka Genap";
+   }
+   getch();
+}
